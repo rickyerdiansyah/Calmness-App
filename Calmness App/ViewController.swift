d8356601f42595548ae9ba8aa6b418597dbb0a19
Ricky Erdiansyah <erdiansyah.ricky@gmail.com>
2019-05-13 08:13:04 +0700

    Initial Commit

---
+++ Calmness App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calmness App
+//
+//  Created by Ricky Erdiansyah on 13/05/19.
+//  Copyright © 2019 Ricky Erdiansyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
